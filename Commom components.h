c0c5fc846ddecb423210e881c60f49771929a2d9
NeverlandC <77274600+CaoZongJing@users.noreply.github.com>
2023-06-06 00:23:54 +0800

    Add files via upload

---
+++ Commom components.h
@@ -0,0 +1,245 @@
+/*
+ * Copyright (c) 1997
+ * Silicon Graphics Computer Systems, Inc.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.  Silicon Graphics makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ */
+
+#pragma once
+
+#include<bits/stdc++.h>
+#include<type_traits>
+#include<iterator>
+
+#define P_TYPE_INTEGRAL_CONST(_Ty)                                  \
+    template<_Ty val>                                               \
+    struct integral_const<_Ty,val>{                                 \
+    using value_type = _Ty;                                         \
+    using type = integral_const;                                    \
+    static constexpr value_type  value = val;                       \
+    using init_type = integral_const<_Ty,val>;                      \
+    using point_type = integral_const<_Ty, val - 1>;                \
+    using next_type = integral_const<_Ty, val + 1>;                 \
+    };
+
+
+/* Comom arithmetic */
+#define P_MULT_ARITHMETIC_OP(name, op)                                         \
+  template <class N1, class N2, class... Nargs>                                \
+  struct name {                                                                \
+    using inner = name<N2, Nargs...>;                                          \
+    using value_type = typename largest_int<typename N1::value_type,           \
+                                            typename inner::value_type>::type; \
+    using type = integral_const<value_type, N1::value op inner::value>;        \
+    static constexpr value_type value = type::value;                           \
+  };                                                                           \
+  template <class N1, class N2>                                                \
+  struct name<N1, N2> {                                                        \
+    using value_type = typename largest_int<typename N1::value_type,           \
+                                            typename N2::value_type>::type;    \
+    using type = integral_const<value_type, (N1::value op N2::value)>;         \
+    static constexpr value_type value = type::value;                           \
+  };
+
+
+// some arithmetic only make non-mult meta-function for such which.
+#define P_ONE_ARITHMETIC_OP(name,op)                                        \
+  template <class N1, class N2>                                             \
+  struct name {                                                             \
+    using value_type = typename largest_int<typename N1::value_type,        \
+                                            typename N2::value_type>::type; \
+    using type = integral_const<value_type, (N1::value op N2::value)>;      \
+    static constexpr value_type value = type::value;                        \
+  };
+
+
+/* relational operation */
+#define P_ONE_RELATIONAL_OP_BOOL(name, op)                             \
+  template <class N1, class N2>                                        \
+  struct name {                                                        \
+    using value_type = bool;                                           \
+    using type = integral_const<value_type, (N1::value op N2::value)>; \
+    static constexpr value_type value = type::value;                   \
+  };
+
+
+/* Mult logical */
+#define P_MULT_RELATIONAL_OP_BOOL(name,op)                                    \
+  template<class _Ty, class _Tys, class... _Args>                             \
+  struct name {                                                               \
+    using value_type = bool;                                                  \
+    using liner = name<_Tys, _Args...>;                                       \
+    using type = integral_const<value_type, (_Ty::value op liner::value)>;    \
+    static constexpr value_type value = type::value;                          \
+  };                                                                          \
+  template<class _Ty, class _Tys>                                             \
+  struct name<_Ty, _Tys> {                                                    \
+      using value_type = bool;                                                \
+      using type = integral_const<value_type, (_Ty::value op _Tys::value)>;   \
+      static constexpr value_type value = type::value;                        \
+  };
+
+
+namespace yampl {
+
+  template<class _Ty1, class _Ty2, class _Ty3>
+  struct type_convert {};
+
+  // change type of function
+  template<class _Ty, class _By_Ty>
+  struct type_convert < _Ty, _Ty, _By_Ty> { using type = _By_Ty; };
+
+
+  template<class _Ty, _Ty val>struct integral_const {};
+
+  //* exist head and next
+  template<bool val>
+  struct integral_const<bool, val> {
+    using value_type = bool;
+    using type = integral_const;
+    static constexpr value_type value = val;
+  };
+
+
+  P_TYPE_INTEGRAL_CONST(int);
+  P_TYPE_INTEGRAL_CONST(unsigned int);
+  P_TYPE_INTEGRAL_CONST(long long);
+
+
+  template<int N>    using int_ = integral_const<int, N>;
+
+  template<unsigned int N>using uint_ = integral_const<unsigned int, N>;
+
+  template<long long N>using ll_ = integral_const<long long, N>;
+
+  template<short N>  using short_ = integral_const<short, N>;
+
+  template<bool N>  using bool_ = integral_const<bool, N>;
+
+
+  template<class... Args>
+  struct seq {};
+  template<class _S, class _T>struct seq_push_back;
+
+  template<class _T, class... Args>
+  struct seq_push_back<seq<Args...>, _T> {
+    using type = seq<Args..., _T>;
+  };
+
+
+  using true_type = bool_<true>;
+  using false_type = bool_<false>;
+
+
+  template<bool Judge, class _T, class _Ty>
+  struct if_bool_ { using type = _T; };
+
+  template<class _T, class _Ty>
+  struct if_bool_<false, _T, _Ty> { using type = _Ty; };
+
+  template<class Judge_space, class _T, class _Ty>
+  struct if_ {
+    using type = typename if_bool_<!!Judge_space::value, _T, _Ty>::type;
+  };
+
+
+  //* Type grade
+
+  template <typename T>
+  struct integral_rank;
+
+  template <> struct integral_rank<bool> : uint_<1> {};
+  template <> struct integral_rank<signed char> : uint_<2> {};
+  template <> struct integral_rank<char> : uint_<3> {};
+  template <> struct integral_rank<unsigned char> : uint_<4> {};
+  template <> struct integral_rank<wchar_t> : uint_<5> {};
+  template <> struct integral_rank<char16_t> : uint_<6> {};
+  template <> struct integral_rank<short> : uint_<7> {};
+  template <> struct integral_rank<unsigned short> : uint_<8> {};
+  template <> struct integral_rank<char32_t> : uint_<9> {};
+  template <> struct integral_rank<int> : uint_<10> {};
+  template <> struct integral_rank<unsigned int> : uint_<11> {};
+  template <> struct integral_rank<long> : uint_<12> {};
+  template <> struct integral_rank<unsigned long> : uint_<13> {};
+  template <> struct integral_rank<long long> : uint_<14> {};
+  template <> struct integral_rank<unsigned long long> : uint_<15> {};
+
+
+  // since if_ immediate using value such will cannot operator in non-computea field.
+  template <class T1, class T2>
+  using largest_int = if_bool_<integral_rank<T1>::value
+    >=
+    integral_rank<T2>::value, T1, T2>;
+
+
+  namespace Arithmetic {
+
+    template <class T>
+    struct negate {
+      using value_type = typename T::value_type;
+      using type = integral_const<value_type, -T::value>;
+      static constexpr value_type value = type::value;
+    };
+
+    P_MULT_ARITHMETIC_OP(plus, +);
+    P_MULT_ARITHMETIC_OP(sub, -);
+    P_MULT_ARITHMETIC_OP(mult, *);
+    P_MULT_ARITHMETIC_OP(bitand_, &);
+    P_MULT_ARITHMETIC_OP(bitor_, | );
+    P_MULT_ARITHMETIC_OP(bitxor_, ^);
+
+    P_ONE_ARITHMETIC_OP(minus, *);
+    P_ONE_ARITHMETIC_OP(divides, / );
+    P_ONE_ARITHMETIC_OP(modulus, %);
+    P_ONE_ARITHMETIC_OP(left_shift, << );
+    P_ONE_ARITHMETIC_OP(right_shift, >> );
+
+  };
+
+
+  namespace logical {
+
+    P_ONE_RELATIONAL_OP_BOOL(equal_to, == );
+    P_ONE_RELATIONAL_OP_BOOL(not_equal_to, != );
+    P_ONE_RELATIONAL_OP_BOOL(greater, > );
+    P_ONE_RELATIONAL_OP_BOOL(greater_equal, >= );
+    P_ONE_RELATIONAL_OP_BOOL(less, < );
+    P_ONE_RELATIONAL_OP_BOOL(less_equal, <= );
+
+    P_MULT_RELATIONAL_OP_BOOL(and_, &&);
+    P_MULT_RELATIONAL_OP_BOOL(or_, || );
+
+    template<class _Ty>
+    struct not_ {
+      using value_type = bool;
+      using type = integral_const<value_type, !_Ty::value>;
+      static constexpr value_type value = type::value;
+    };
+
+  };
+
+
+  template <class T>
+  struct func_helper_type {
+    using cond = typename logical::or_<typename std::is_scalar<T>::type,
+      typename std::is_reference<T>::type>;
+
+    using type = typename if_<typename cond::type, T,
+      typename std::add_lvalue_reference<T>::type>::type;
+  };
+
+
+  template<class _Ty>struct is_refence_ { static constexpr bool value = false; };
+
+  template<class _Ty> struct is_refence_<_Ty&> {
+    static constexpr bool value = true;
+  };
+
+
+};    // namespace yampl
---
+++ List Compile.h
@@ -0,0 +1,425 @@
+/*
+ * Copyright (c) 1997
+ * Silicon Graphics Computer Systems, Inc.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.  Silicon Graphics makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ */
+
+#include "Commom components.h"
+
+namespace yampl {
+    struct iterator_tag {};
+
+    template<class _Ty, class _Tn, class _B = void, class _BB = void, class _BBB = void>
+    struct iterator {
+        using type = _Ty;     using index = _Tn;
+        using backup = _B;     using backup2 = _BB;
+        using backup3 = _BBB;
+
+        //* By compatible bedefine such non-template of data structure
+        using tag = iterator_tag;
+    };
+
+    template <class T>
+    struct sequence_tag {
+        using type = typename T::tag;
+    };
+
+    struct forward_iterator_tag {};
+
+    template <class _Ty>
+    struct iterator_sequence_tag {
+        using type = typename _Ty::tag;
+    };
+
+
+    template <class Tag>    struct begin_impl {};
+    template <class Tag>    struct end_impl {};
+
+    template <class Tag>    struct size_impl {};
+    template<class _Tag>    struct insert_impl {};
+    template<class _Tag>    struct index_impl {};
+
+
+    template <class Tag>    struct next_impl {};
+    template <class Tag>    struct prior_impl {};
+
+    template <class Tag>    struct deref_impl {};
+    template <class Tag>    struct clear_impl {};
+
+
+    template <class Tag>    struct advance_impl {};
+    template <class Tag>    struct distance_impl {};
+
+
+    template <class Tag>    struct push_back_impl {};
+    template<class _Tag>    struct push_front_impl {};
+    template<class _Tag>    struct pop_back_impl {};
+    template<class _Tag>    struct pop_front_impl {};
+
+
+    template<class _Tag>    struct sort_impl {};
+
+
+    template <class T> struct begin
+        : begin_impl<typename sequence_tag<T>::type>::template apply<T> {};
+
+    template <class T> struct end
+        : end_impl<typename sequence_tag<T>::type>::template apply<T> {};
+
+    template <class T> struct size
+        : size_impl<typename sequence_tag<T>::type>::template apply<T> {};
+
+
+    template <class T> struct next
+        : next_impl<typename iterator_sequence_tag<T>::type>::template apply<T> {};
+
+    template <class T> struct prior
+        : prior_impl<typename iterator_sequence_tag<T>::type>::template apply<T> {};
+
+    template <class T> struct deref
+        : deref_impl<typename iterator_sequence_tag<T>::type>::template apply<T> {};
+
+
+    template <class T> struct clear
+        : clear_impl<typename sequence_tag<T>::type>::template apply<T> {};
+
+
+    template <class T, class N> struct advance
+        : advance_impl<typename iterator_sequence_tag<T>::type>::template
+        apply<T, N> {};
+
+
+    template <class T, class U> struct distance
+        : distance_impl<typename iterator_sequence_tag<T>::type>::template apply<
+        T, U> {};
+
+
+    template <class T, class U> struct push_back
+        : push_back_impl<typename sequence_tag<T>::type>::template apply<T, U> {};
+
+    template<class _Ty, class _Tv>
+    struct push_front
+        : push_front_impl<typename sequence_tag<_Ty>::type>::template apply<_Ty, _Tv>{};
+
+
+    template<class _Ty> struct pop_back
+        : pop_back_impl<typename sequence_tag<_Ty>::type>::template apply<_Ty>{};
+
+
+    template<class _Ty>
+    struct pop_front
+        : pop_front_impl<typename sequence_tag<_Ty>::type>::template apply<_Ty>{};
+
+
+    template <class T, class U, class B>
+    struct insert
+        : insert_impl<typename sequence_tag<T>::type>::template apply<T, B, U> {};
+
+
+    template<class _Ty, class _N>
+    struct index
+        : index_impl<typename iterator_sequence_tag<_Ty>::type>::template apply<_Ty, _N>{};
+
+    template<class _Ty, int N>
+    struct index_value
+        : index_impl<typename iterator_sequence_tag<_Ty>::type>::template apply_value<_Ty, N>{};
+
+
+    template<class _Ty>struct sort
+        : sort_impl<typename sequence_tag<_Ty>::type>::template apply<_Ty>{};
+
+
+    struct list_tag {};
+
+    template <class... Args>
+    struct list {
+        using iterator_category_tag = forward_iterator_tag;
+        using tag = list_tag;
+    };
+
+    template<>
+    struct begin_impl<list_tag> {
+        template<class _Ty>
+        struct apply;
+
+        template<template<class...>class _Ty, class... _args>
+        struct apply<_Ty<_args...>> {
+            using type = iterator<_Ty<_args...>,
+                integral_const<int, 0>, _Ty<_args...>>;
+        };
+    };
+
+    template<>
+    struct end_impl<list_tag> {
+        template<class _Ty>
+        struct apply;
+
+        template<class _Ty>
+        struct apply_element;
+
+        template<template<class...>class _Ty, class... _args>
+        struct apply<_Ty<_args...>> {
+            using type = iterator<_Ty<>,
+                integral_const<int, sizeof...(_args)>,
+                _Ty<_args...>>;
+        };
+    };
+
+
+    template <>
+    struct size_impl<list_tag> {
+        template <class _Ty>
+        struct apply;
+
+        template <template <class...> class _Ty, class... _args>
+        struct apply<_Ty<_args...>> {
+            using type = integral_const<int, sizeof...(_args)>;
+        };
+    };
+
+
+    template<>
+    struct clear_impl<list_tag> {
+        template<class _Ty>
+        struct apply;
+
+        template <template <class...> class _Ty, class... _args>
+        struct apply<_Ty<_args...>> {
+            using type = _Ty<>;
+        };
+    };
+
+
+    template <>
+    struct push_back_impl<list_tag> {
+        template <class _Ty, class _Tv>
+        struct apply;
+
+        template <template <class...> class _Ty, class _Tv, class... _args>
+        struct apply<_Ty<_args...>, _Tv> {
+            using type = _Ty<_args..., _Tv>;
+        };
+    };
+
+
+    template<>
+    struct next_impl<iterator_tag> {
+        template<class _Ty>
+        struct apply;
+
+        template<template<class...>class _Ty, class _Tn, class _Tv, class _Tp, class... _args>
+        struct apply<iterator<_Ty<_Tv, _args...>, _Tn, _Tp>> {
+            using type = iterator<_Ty<_args...>, typename _Tn::next_type, _Tp>;
+        };
+
+        //* only one
+        template<template<class...>class _Ty, class _Tn, class _Tv, class _Tp>
+        struct apply<iterator<_Ty<_Tv>, _Tn, _Tp>> {
+            using type = iterator<_Ty<>, typename _Tn::next_type, _Tp>;
+        };
+    };
+
+
+    struct none_tag {};
+    struct none { using tag = none_tag; };
+
+
+    template<>
+    struct deref_impl<iterator_tag> {
+        template<class _Ty>
+        struct apply;
+
+        template<template<class...>class _Ty, class _Tn, class _Tv,
+            class _Tp, class... _args>
+        struct apply<iterator<_Ty<_Tv, _args...>, _Tn, _Tp>> {
+            using type = _Tv;
+        };
+
+        template<template<class...>class _Ty, class _Tn, class _Tp>
+        struct apply<iterator<_Ty<>, _Tn, _Tp>> {
+            using type = none;
+        };
+    };
+
+
+
+    template<>
+    struct push_front_impl<list_tag> {
+        template<class _Ty, class _Tv_begin, class _Tv_end>
+        struct apply_impl {
+            using inner = typename push_back<_Ty, typename deref<_Tv_begin>::type>::type;
+            using type = typename apply_impl<inner, typename next<_Tv_begin>::type, _Tv_end>::type;
+        };
+
+        template<class _Ty, class _Tv_end>
+        struct apply_impl<_Ty, _Tv_end, _Tv_end> { using type = _Ty; };
+
+        template<class _Ty, class _Tv>
+        struct apply {
+            using init = typename push_back<typename clear<_Ty>::type, _Tv>::type;
+            using type = typename apply_impl<init,
+                typename begin<_Ty>::type,
+                typename end<_Ty>::type>::type;
+        };
+    };
+
+
+    template<>
+    struct pop_back_impl<list_tag> {
+        template<class _Ty, class _Tv_begin, class _Tv_N, class _Tv_end>
+        struct apply_impl {
+            using inner = typename push_back<_Ty, typename deref<_Tv_begin>::type>::type;
+            using type = typename apply_impl<inner, typename next<_Tv_begin>::type,
+                typename next<_Tv_N>::type, _Tv_end>::type;
+        };
+
+        template<class _Ty, class _Tv_P, class _Tv_end>
+        struct apply_impl<_Ty, _Tv_P, _Tv_end, _Tv_end> { using type = _Ty; };
+
+        template<class _Ty>
+        struct apply {
+            using init = typename clear<_Ty>::type;
+            using type = typename apply_impl<init,
+                typename begin<_Ty>::type,
+                typename next<typename begin<_Ty>::type>::type,
+                typename end<_Ty>::type>::type;
+        };
+    };
+
+    template<>
+    struct pop_front_impl<list_tag> {
+        template<class _Ty, class _Tv_begin, class _Tv_end>
+        struct apply_impl {
+            using inner = typename push_back<_Ty, typename deref<_Tv_begin>::type>::type;
+            using type = typename apply_impl<inner, typename next<_Tv_begin>::type, _Tv_end>::type;
+        };
+
+        template<class _Ty, class _Tv_end>
+        struct apply_impl<_Ty, _Tv_end, _Tv_end> { using type = _Ty; };
+
+        template<class _Ty>
+        struct apply {
+            using init = typename clear<_Ty>::type;
+            using type = typename apply_impl<init,
+                typename next<typename begin<_Ty>::type>::type,
+                typename end<_Ty>::type>::type;
+        };
+    };
+
+
+    template<>
+    struct insert_impl<list_tag> {
+        template <class R, class U, class B, class I, class E>
+        struct apply_impl {
+            using inner = typename push_back<R, typename deref<I>::type>::type;
+            using type =
+                typename apply_impl<inner, U, B, typename next<I>::type, E>::type;
+        };
+
+        template <class R, class U, class I, class E>
+        struct apply_impl<R, U, I, I, E> {
+            using inner = typename push_back<R, U>::type;
+            using inner2 = typename push_back<inner, typename deref<I>::type>::type;
+            using type = typename apply_impl<inner2, I, U, typename next<I>::type, E>::type;
+        };
+
+        template <class R, class U, class B, class E>
+        struct apply_impl<R, U, B, E, E> {
+            using type = R;
+        };
+
+        template <class R, class U, class E>
+        struct apply_impl<R, U, E, E, E> {
+            using type = typename push_back<R, U>::type;
+        };
+
+        template <class T, class B, class U>
+        struct apply {
+            using init = typename clear<T>::type;
+            using type = typename apply_impl<init, U, B, typename begin<T>::type,
+                typename end<T>::type>::type;
+        };
+    };
+
+
+    template<>
+    struct index_impl<list_tag> {
+
+        template<class _Ty, int N>
+        struct apply_impl { using type = typename apply_impl<typename next<_Ty>::type, N - 1>::type; };
+
+        template<class _Ty> struct apply_impl<_Ty, 0> { using type = _Ty; };
+
+
+        template<class _Ty, class _N>struct apply {
+            using type = typename apply_impl<typename begin<_Ty>::type, _N::value>::type;
+            using deref_type = typename deref<type>::type;
+        };
+
+        template<class _Ty, int _N>
+        struct apply_value {
+            using type = typename apply_impl<typename begin<_Ty>::type, _N>::type;
+            using deref_type = typename deref<type>::type;
+        };
+    };
+
+
+    // TODO
+    template<>
+    struct sort_impl<list_tag> {
+
+        template<class _Ty, class _Tv, class _Tn, class _To, class _Tp, class _TY>
+        struct apply_impl {
+            using trans = _Tv;  using type = _Ty;
+            using _PP = typename begin<_Ty>::type;
+
+      //* ERROR 
+            using get = typename if_<typename logical
+                ::greater<typename deref<trans>::type, typename deref<_Tn>::type>::type,
+                typename apply_impl<type, _Tn, trans, typename next<_To>::type, _Tp, _TY>::get,
+                typename apply_impl<type, trans, typename next<_Tn>::type, _To, _Tp, _TY>::get>::type;
+        };
+        template<class _Ty, class _Tv, class _Tn, class _Tp, class _TY>
+        struct apply_impl<_Ty, _Tv, _Tn, _Tp, _Tp, _TY> {
+            using _PP = typename begin<_Ty>::type;
+            using trans = typename next<_Tv>::type;
+            using type = typename push_back<_Ty, typename deref<_Tv>::type>::type;
+
+            using get = typename if_<typename
+                logical::greater<typename deref<trans>::type, typename deref<_Tn>::type>::type,
+                typename apply_impl<type, _Tn, trans, _PP, _Tp, _TY>::get,
+                typename apply_impl<type, trans, typename next<_Tn>::type, _PP, _Tp, _TY>::get>::type;
+        };
+
+        template<class _Ty, class _Tv, class _Tp, class _TY>
+        struct apply_impl<_Ty, _Tv, _Tp, _Tp, _Tp, _TY> {
+
+            using get = typename if_<typename logical::greater<typename deref<_Tv>::type,
+                typename deref<typename next<_Tv>::type>::type>::type, typename next<_Tv>::type, _Tv>::type;
+
+            using type = typename push_back<_Ty, typename deref<get>::type>::type;
+        };
+
+
+        template<class _Ty>
+        struct apply {
+            using init = typename clear<_Ty>::type;
+            using iter_start = typename begin<_Ty>::type;
+            using iter_end = typename index<_Ty, typename size<_Ty>::type>::type;
+            using type = typename apply_impl<init,
+                iter_start,
+                typename next<iter_start>::type,
+                iter_end,
+                typename end<_Ty>::type, _Ty>::type;
+        };
+    };
+
+
+};      // namespace yampl
